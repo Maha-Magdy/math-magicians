12dde609a31037409afc345cde55b7ecee16005c
Maha Magdy <mahamagdy3@gmail.com>
2021-09-24 02:42:13 +0200

    Add the logic needed to make the Calculator component work

--- src/App.js
+++ src/App.js
@@ -4,12 +4,41 @@
 import React, { Component } from 'react';
 import './App.css';
 import Calculator from './components/Calculator.js';
+import calculate from './logic/calculate.js';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      dataObj: {},
+      CalculatorScreen: 0,
+    };
+  }
+
+  handleClick = (event) => {
+    const buttonValue = event.target.innerText;
+    this.setState(
+      (state) => ({
+        dataObj: calculate(state.dataObj, buttonValue),
+      }),
+      function () {
+        console.log(buttonValue, this.state.dataObj);
+        const CalculatorScreen = this.state.dataObj.next
+          ? this.state.dataObj.next
+          : this.state.dataObj.total;
+        this.setState({
+          CalculatorScreen,
+        });
+        console.log(this.state.CalculatorScreen);
+      },
+    );
+  };
+
   render() {
     return (
       <div className="App">
-        <Calculator />
+        <Calculator handleClick={this.handleClick} CalculatorScreen={this.state.CalculatorScreen}/>
       </div>
     );
   }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,36 +5,36 @@ export default class Calculator extends React.Component {
   render() {
     return (
       <div className="calculator-container">
-        <input type="text" placeholder="0"></input>
+        <input type="text" value={this.props.CalculatorScreen}></input>
         <table>
           <tr>
-            <td className="bg-light-gray">AC</td>
-            <td className="bg-light-gray">+/-</td>
-            <td className="bg-light-gray">%</td>
-            <td className="bg-orange">÷</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>AC</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>+/-</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>%</td>
+            <td className="bg-orange" onClick={this.props.handleClick}>÷</td>
           </tr>
           <tr>
-            <td className="bg-light-gray">7</td>
-            <td className="bg-light-gray">8</td>
-            <td className="bg-light-gray">9</td>
-            <td className="bg-orange">×</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>7</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>8</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>9</td>
+            <td className="bg-orange" onClick={this.props.handleClick}>x</td>
           </tr>
           <tr>
-            <td className="bg-light-gray">4</td>
-            <td className="bg-light-gray">5</td>
-            <td className="bg-light-gray">6</td>
-            <td className="bg-orange">-</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>4</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>5</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>6</td>
+            <td className="bg-orange" onClick={this.props.handleClick}>-</td>
           </tr>
           <tr>
-            <td className="bg-light-gray">1</td>
-            <td className="bg-light-gray">2</td>
-            <td className="bg-light-gray">3</td>
-            <td className="bg-orange">+</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>1</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>2</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>3</td>
+            <td className="bg-orange" onClick={this.props.handleClick}>+</td>
           </tr>
           <tr>
-            <td className="bg-light-gray" colSpan="2">0</td>
-            <td className="bg-light-gray">.</td>
-            <td className="bg-orange">=</td>
+            <td className="bg-light-gray" colSpan="2" onClick={this.props.handleClick}>0</td>
+            <td className="bg-light-gray" onClick={this.props.handleClick}>.</td>
+            <td className="bg-orange" onClick={this.props.handleClick}>=</td>
           </tr>
         </table>
       </div>
