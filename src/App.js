9efb2bb8e0cf17586c547cbd0119a9a742936419
Maha Magdy <mahamagdy3@gmail.com>
2021-10-02 03:14:28 +0200

    Add the style of the application, and routes to manage the views

--- src/App.js
+++ src/App.js
@@ -2,15 +2,50 @@
 /* eslint-disable  no-unused-vars */
 
 import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route, Link,
+} from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator.js';
+import Home from './components/Home.js';
+import Quote from './components/Quote.js';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Calculator/>
-      </div>
+      <Router>
+        <div className="App">
+          <header>
+            <h1>
+              <Link to="/" className="color-orange">Math Magicians</Link>
+            </h1>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/" className="color-orange">Home</Link>
+                </li>
+                <li>
+                  <Link to="/Calculator" className="color-orange">Calculator</Link>
+                </li>
+                <li>
+                  <Link to="/quote" className="color-orange">Quote</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <Switch>
+            <Route path="/Calculator">
+              <Calculator />
+            </Route>
+            <Route path="/quote">
+              <Quote />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+const Home = () => (
+  <div style={{ width: '75%' }}>
+    <h2 style={{ color: '#858694' }}>Welcome to our page!</h2>
+    <p style={{ color: '#858694' }}>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi soluta
+      eveniet sunt, cupiditate, possimus quam iste quae quo mollitia obcaecati
+      delectus velit deleniti saepe perferendis. Cum, autem quisquam. Beatae,
+      tempora! Ipsum placeat minima deleniti quidem doloribus nam veritatis
+      minus. Tempora assumenda nesciunt molestias eos doloremque! Quasi adipisci
+      eveniet corrupti modi?
+    </p>
+    <p style={{ color: '#858694' }}>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque omnis
+      delectus explicabo placeat cupiditate magnam mollitia quod quasi numquam
+      laborum culpa vel cumque voluptas enim accusantium, nihil quos eaque magni
+      perspiciatis qui aspernatur libero id perferendis velit? In eum animi nemo
+      reiciendis omnis sit accusantium, cumque aliquid repudiandae mollitia
+      eligendi, earum ab dolore veritatis voluptate? Aliquam quos tenetur
+      laboriosam reprehenderit!
+    </p>
+  </div>
+);
+
+export default Home;
